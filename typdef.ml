27cd5fbadc31ff0bb7666721ff93ba7737cff04f
Matthias Puech <puech@cs.unibo.it>
2011-03-03 15:19:51 +0100

    Pass [open] directives from module_types to module_expr

--- typdef.ml
+++ typdef.ml
@@ -51,12 +51,13 @@ and types path sigma : module_type -> module_expr = function
       let l = List.fold_right
 	(fun si acc ->
 	   match si with
+	     | SgOpn(loc, os) -> StOpn(loc, os) :: acc
 	     | SgTyp(loc, tds) -> 
 		 StTyp(loc, subst_tds path sigma tds) :: acc
 	     | SgMod(loc, r, mts) ->
-		 (try StMod(loc, r, subst_mts path sigma r mts) :: acc
-		  with Invalid_argument _ -> acc)
-	     | SgMty(loc, n, mt) -> StMty(loc, n, mt) :: acc
+ 		 (try StMod(loc, r, subst_mts path sigma r mts) :: acc
+ 		  with Invalid_argument _ -> acc)
+ 	     | SgMty(loc, n, mt) -> StMty(loc, n, mt) :: acc
 	     | SgExc(loc, n, ts) -> StExc(loc, n, ts, []) :: acc
 	     | _ -> acc
 	) items [] in
